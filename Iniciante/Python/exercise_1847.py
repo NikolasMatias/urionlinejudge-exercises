dc5035e4707fe8c4f394e4839fd0eb29241ce664
Nikolas Matias <nikolas.matias500@gmail.com>
2023-07-30 19:37:26 -0300

    Fazendo alguns exercicios em python

--- Iniciante/Python/exercise_1847.py
+++ Iniciante/Python/exercise_1847.py
@@ -21,17 +21,21 @@ def calculateResult(valor_a, valor_b, valor_c):
             'answer': ':)'
         },
         {
-            'result': lambda: valor_a > valor_b and valor_b < valor_c,
+            'result': lambda: valor_a > valor_b and valor_b > valor_c and (valor_b-valor_c > valor_a-valor_b),
+            'answer': ':('
+        },
+        {
+            'result': lambda: valor_a == valor_b and valor_b < valor_c,
             'answer': ':)'
         },
         {
-            'result': lambda: valor_a > valor_b and valor_b < valor_c,
-            'answer': ':)'
-        },
-        {
-            'result': lambda: valor_a > valor_b and valor_b < valor_c,
-            'answer': ':)'
-        },
+            'result': lambda: valor_a == valor_b and valor_b > valor_c,
+            'answer': ':('
+        }
     ]
+    for possibility in possibilities:
+        if possibility['result']():
+            return possibility['answer']
 
 valor_a, valor_b, valor_c = [int(x) for x in input().split()]
+print(calculateResult(valor_a, valor_b, valor_c))
