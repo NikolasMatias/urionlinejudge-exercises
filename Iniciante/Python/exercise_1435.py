3695f69dc4a75c60f04b82fa83b199d81fb6165c
Nikolas Matias <nikolas.matias500@gmail.com>
2023-07-30 03:14:35 -0300

    Fazendo alguns exercicios em python

--- Iniciante/Python/exercise_1435.py
+++ Iniciante/Python/exercise_1435.py
@@ -2,30 +2,24 @@ while True:
     countLoop = int(input())
     if countLoop == 0:
         break
-    matriz = []
-    for x in range(countLoop):
-        matriz.append([])
-
-    for x in range(countLoop):
-        for y in range(countLoop):
-            matriz[x].append(1)
 
     for x in range(countLoop):
         for y in range(countLoop):
             baseX = x+1
             baseY = y+1
-            baseValue = min([x,y])+1
-            matriz[x][y] = baseValue
-
-            if x == 4 and y == 3:
-                teste = 1
+            baseValue = x
+            if y < x:
+                baseValue = y
+            baseValue += 1
 
             if (countLoop - baseX + 1) < baseValue:
-                matriz[x][y] = countLoop - baseX + 1
+                baseValue = countLoop - baseX + 1
 
             if (countLoop - baseY + 1) < baseValue:
-                matriz[x][y] = countLoop - baseY + 1
+                baseValue = countLoop - baseY + 1
 
-
-    for x in range(countLoop):
-        print(' '.join([str(y) for y in matriz[x]]).strip())
+            print(str(baseValue), end='')
+            if baseY < countLoop:
+                print(' ', end='')
+            else:
+                print('')
