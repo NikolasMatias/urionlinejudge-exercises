58561781b4781f4b556b7ec4ef29f3b0931d40fd
Nikolas Matias <nikolas.matias500@gmail.com>
2023-07-31 03:11:07 -0300

    Fazendo alguns exercicios em python

--- Iniciante/Python/exercise_1847.py
+++ Iniciante/Python/exercise_1847.py
@@ -1,41 +1,20 @@
-def calculateResult(valor_a, valor_b, valor_c):
-    possibilities = [
-        {
-            'result': lambda: valor_a > valor_b and valor_b < valor_c,
-            'answer': ':)'
-        },
-        {
-            'result': lambda: valor_a < valor_b and valor_b > valor_c,
-            'answer': ':('
-        },
-        {
-            'result': lambda: valor_a < valor_b and valor_b < valor_c and (valor_c-valor_b < valor_b-valor_a),
-            'answer': ':('
-        },
-        {
-            'result': lambda: valor_a < valor_b and valor_b < valor_c and (valor_c-valor_b >= valor_b-valor_a),
-            'answer': ':)'
-        },
-        {
-            'result': lambda: valor_a > valor_b and valor_b > valor_c and (valor_b-valor_c < valor_a-valor_b),
-            'answer': ':)'
-        },
-        {
-            'result': lambda: valor_a > valor_b and valor_b > valor_c and (valor_b-valor_c > valor_a-valor_b),
-            'answer': ':('
-        },
-        {
-            'result': lambda: valor_a == valor_b and valor_b < valor_c,
-            'answer': ':)'
-        },
-        {
-            'result': lambda: valor_a == valor_b and valor_b > valor_c,
-            'answer': ':('
-        }
-    ]
-    for possibility in possibilities:
-        if possibility['result']():
-            return possibility['answer']
-
 valor_a, valor_b, valor_c = [int(x) for x in input().split()]
-print(calculateResult(valor_a, valor_b, valor_c))
+
+if valor_a > valor_b:
+    if valor_c > valor_b:
+        print(':)')
+    elif valor_b - valor_c < valor_a - valor_b:
+        print(':)')
+    else:
+        print(':(')
+elif valor_a < valor_b:
+    if valor_c < valor_b:
+        print(':(')
+    elif valor_b - valor_c > valor_a - valor_b:
+        print(':(')
+    else:
+        print(':)')
+elif valor_c > valor_a:
+        print(':)')
+else:
+    print(':(')
