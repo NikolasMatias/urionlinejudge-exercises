7a6909c0cf4206ac8e0281ef3aaf5214660e7726
Nikolas Matias <nikolas.matias500@gmail.com>
2023-08-01 00:26:19 -0300

    Fazendo alguns exercicios em python

--- Iniciante/Python/exercise_1858.py
+++ Iniciante/Python/exercise_1858.py
@@ -1,17 +1,11 @@
 quantidade_n = int(input())
 valores = [int(x) for x in input().split()]
 resposta_certa = 0
-repete = False
-valor_repete = 0
-for x in range(1, quantidade_n):
-    if valores[x] < valores[x-1]:
-        resposta_certa = x+1
-    elif valores[x] == valores[x-1]:
-        repete = True
-        valor_repete = valores[x]
-        resposta_certa = x
-for x in range(1, quantidade_n):
-    if valor_repete == valores[x]:
+minValue = min(valores)
+
+for x in range(quantidade_n):
+    if valores[x] == minValue:
         resposta_certa = x+1
         break
+
 print(str(resposta_certa))
