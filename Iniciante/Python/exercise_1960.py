d174b65acd86504a13099e8e7823dc7cc1350287
Nikolas Matias <nikolas.matias500@gmail.com>
2023-08-05 21:39:51 -0300

    Fazendo alguns exercicios em python

--- Iniciante/Python/exercise_1960.py
+++ Iniciante/Python/exercise_1960.py
@@ -1,11 +1,28 @@
 def int_to_roman(number):
-    int_base = [
-        1000, 900, 500, 400, 100, 90,
-        50, 40, 10, 9, 5, 4, 1
-    ]
-    roman_base = [
-        'M', 'CM', 'D', 'CD', 'C', 'XC', 'L',
-        'XL', 'X', 'IX', 'V', 'IV', 'I'
-    ]
-    for x in int_base:
-        if number // x:
+    result = ''
+    resto = number
+    int_to_roman_base = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
+    keys = list(int_to_roman_base.keys())
+    for x in sorted(keys, reverse=True):
+        n_times = resto // x
+        resto = resto % x
+        if n_times > 0:
+            result = result + int_to_roman_base[x]*n_times
+    return result
+
+valor_a = int(input())
+print(int_to_roman(valor_a))
