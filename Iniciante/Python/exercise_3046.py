ca658ee8b2100e2b687c3a081555fa0770b86198
Nikolas Matias <nikolas.matias500@gmail.com>
2023-08-21 04:07:32 -0300

    Fazendo alguns exercicios em python

---
+++ Iniciante/Python/exercise_3046.py
@@ -0,0 +1,3 @@
+duplo_n = int(input())
+qtde_pecas = ((duplo_n+1)*(duplo_n+2))/2
+print(str(int(qtde_pecas)))
---
+++ Iniciante/Python/exercise_3055.py
@@ -0,0 +1,4 @@
+primeira_prova = int(input())
+media = int(input())
+segunda_prova = media*2-primeira_prova
+print(str(segunda_prova))
